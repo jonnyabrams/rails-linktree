3e34c4978d20ef57e9834bc9d46f5bdf18bae2ae
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-06 16:44:25 +0000

    feat: profile page initial

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'dashboard', to: "dashboard#index"
   root 'dashboard#index'
+
+  # allows us to use user_path(user) to get url for user's profile
+  get ':id', to: 'dashboard#show', as: :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
